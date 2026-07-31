129aac8c24916a5bc0cd1bf2c65b72a915309661
jooh6969 <154743789+jooh6969@users.noreply.github.com>
2026-07-31 15:14:46 +0800

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = [[]]
+        # append modifies ans, but returns none, so i can't do the append
+        # and the return on the same line
+        if len(strs) <= 1:
+            ans[0].append(strs[0])
+            return ans
+        letter_group = []
+        first_letters = Counter(strs[0])
+        letter_group.append(first_letters)
+        ans[0].append(strs[0])
+        for s in strs[1:]:
+            found = False
+            curr_letters = Counter(s)
+            for i, letters in enumerate(letter_group):
+                tmp = i
+                if curr_letters == letters:
+                    ans[i].append(s)
+                    found = True
+                    break
+            if not found:
+                tmp += 1
+                ans.append([])
+                letter_group.append(curr_letters)
+                ans[tmp].append(s)
+        return ans
